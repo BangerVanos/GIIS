cf7dedd110510060eb313f714ac3affce0eb0bee
Raman Zalomau <romchik349@gmail.com>
2024-03-05 13:08:15 +0300

    Minor changer

--- GraphicsRedactor/src/graphics_redactor_backend_api.py
+++ GraphicsRedactor/src/graphics_redactor_backend_api.py
@@ -6,3 +6,6 @@ class Drawer:
 
     def __init__(self, canvas: img.Image) -> None:
         self._canvas: img.Image = canvas
+    
+    def draw_point(self, point: Point, color: str) -> None:
+        pass
